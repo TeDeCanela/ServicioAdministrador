bb017de74f34b51de253a2367770e66012691967
TeDeCanela <153479319+TeDeCanela@users.noreply.github.com>
2024-10-13 21:26:26 -0600

    prueba comentario prueba

--- Host/HostMain.cs
+++ Host/HostMain.cs
@@ -10,7 +10,7 @@ namespace Host
             using (ServiceHost host = new ServiceHost(typeof(ServicioAdministrador.ImplementacionDeServicio)))
             {
                 host.Open();
-                Console.WriteLine("Server is running now");
+                Console.WriteLine("Server is running noooooooooow inglish");
 
                 Console.ReadLine();
             }
