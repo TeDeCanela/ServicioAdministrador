f7ef01e42f1c713191b0faa3ef9c66aa1527fe55
karolkkqg <zs22013661@estudiantes.uv.mx>
2024-10-14 10:25:27 -0600

    cambios en IServicioAdministradorCallback

--- CalculatorService/IServicioAdministrador.cs
+++ CalculatorService/IServicioAdministrador.cs
@@ -5,14 +5,14 @@ using System.ServiceModel;
 namespace ServicioAdministrador
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICalculatorService" in both code and config file together.
-    [ServiceContract(CallbackContract =typeof(ICalculatorServiceCallback))]
+    [ServiceContract(CallbackContract =typeof(IServicioAdministradorCallback))]
     public interface IServicioAdministrador
     {
         [OperationContract(IsOneWay = true)]
         void Sum(int a, int b);
     }
     [ServiceContract]
-    public interface ICalculatorServiceCallback
+    public interface IServicioAdministradorCallback
     {
         [OperationContract]
         void Response(int result);
